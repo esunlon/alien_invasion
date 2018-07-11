85ff0f0a930db1843bf0394b06253632d563142e
esunlon <acousticsung@gmail.com>
2018-07-11 09:58:13 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+	"""A class to store all settings for Alien Invasion."""
+	
+	def __init__(self):
+		"""Init the game's settings."""
+		# Screen settings
+		self.screen_width = 600
+		self.screen_height = 400
+		self.bg_color = (230, 230, 230)
+		
+		# Ship Settings
+		self.ship_speed_factor = 1.5
+		
---
+++ ship.py
@@ -0,0 +1,33 @@
+import pygame
+
+class Ship():
+	def __init__(self, screen):
+		"""Initialize the ship and set its strating position."""
+		self.screen = screen
+		
+		# load the ship image and get its rect.
+		self.image = pygame.image.load('images/ship.bmp')
+		self.rect = self.image.get_rect()
+		self.screen_rect = screen.get_rect()
+		# Start each new ship at the bottom center of the screen.
+		self.rect.centerx = self.screen_rect.centerx
+		self.rect.bottom = self.screen_rect.bottom
+		
+		# Moving flag
+		self.moving_right = False
+		self.moving_left = False
+		
+	def update(self):
+		"""Update the ship's position based on the movement flag."""
+		if self.moving_right:
+			self.rect.centerx += 1
+		if self.moving_left:
+			self.rect.centerx -= 1
+		
+		
+		
+	def blitme(self):
+		"""Draw the ship at its current location."""
+		self.screen.blit(self.image, self.rect)
+		 
+		
